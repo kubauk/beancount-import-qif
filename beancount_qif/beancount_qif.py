39e53d7278622152629ed2057a494d5bf77a0ff3
Jakub Jamro <kuba.jamro@gmail.com>
2018-09-18 21:42:41 +0100

    parse a qif and return some transactions with a single posting

--- beancount_qif/beancount_qif.py
+++ beancount_qif/beancount_qif.py
@@ -1,7 +1,9 @@
 import os
 import re
 
+from beancount.core import data
 from beancount.ingest.importer import ImporterProtocol
+from qifparse.parser import QifParser
 
 
 class Importer(ImporterProtocol):
@@ -15,10 +17,24 @@ class Importer(ImporterProtocol):
         return self._file_name_re.search(abspath) is not None
 
     def extract(self, file, existing_entries=None):
-        pass
+        transactions = list()
+        with open(file.name) as f:
+            parser = QifParser.parse(f)
+            for transaction in parser.get_transactions()[0]:
+                data_transaction = data.Transaction(payee=None,
+                                                date=transaction.date.date(),
+                                                flag=ImporterProtocol.FLAG,
+                                                narration=transaction.payee,
+                                                meta=data.new_metadata(file.name, 0),
+                                                tags=data.EMPTY_SET,
+                                                links=data.EMPTY_SET,
+                                                postings=[])
+                data.create_simple_posting(data_transaction, self.file_account(file), transaction.amount, "GBP")
+                transactions.append(data_transaction)
+        return transactions
 
     def file_account(self, file):
-        pass
+        return None
 
     def file_name(self, file):
         pass
