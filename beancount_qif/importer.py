3303227d3aab3d358933a70b3e55036cb220f265
Jakub Jamro <kuba.jamro@gmail.com>
2021-12-17 21:38:26 +0000

    updating importer to beangulp

--- beancount_qif/importer.py
+++ beancount_qif/importer.py
@@ -1,25 +1,22 @@
+import datetime
 import os
 import re
+from typing import Optional
 
 from beancount.core import data
-from beancount.ingest.importer import ImporterProtocol
 from beancount.utils.defdict import ImmutableDictWithDefault
+from beangulp import Importer, cache
+from beangulp.importer import ImporterProtocol
 from qifparse.parser import QifParser
 
 
-class Importer(ImporterProtocol):
+class QIFImporter(Importer):
     def __init__(self, file_name_mapping=ImmutableDictWithDefault({re.compile(".qif$"): "Expenses:QIF"})) -> None:
         super().__init__()
         self._file_name_mapping = file_name_mapping
 
-    def name(self):
-        return ImporterProtocol.name(self)
-
-    def identify(self, file):
-        for_file = self._mapping_for_file(file)
-        return for_file is not None
-
-    def extract(self, file, existing_entries=None):
+    def extract(self, filepath: str, existing_entries: data.Entries) -> data.Entries:
+        file = cache.get_file(filepath)
         transactions = list()
         with open(file.name) as f:
             parser = QifParser.parse(f)
@@ -33,23 +30,21 @@ class Importer(ImporterProtocol):
                                                     links=data.EMPTY_SET,
                                                     postings=[])
 
-                account = self.file_account(file)
+                account = self.account(filepath)
                 if account:
                     data.create_simple_posting(data_transaction, account, transaction.amount, "GBP")
                 transactions.append(data_transaction)
         return transactions
 
-    def file_account(self, file):
-        return self._mapping_for_file(file)
+    def identify(self, filepath: str) -> bool:
+        for_file = self._mapping_for_file(filepath)
+        return for_file is not None
 
-    def file_name(self, file):
-        pass
+    def account(self, filepath: str) -> data.Account:
+        return self._mapping_for_file(filepath)
 
-    def file_date(self, file):
-        pass
-
-    def _mapping_for_file(self, file):
-        abspath = os.path.abspath(file.name)
+    def _mapping_for_file(self, filepath: str) -> Optional[str]:
+        abspath = os.path.abspath(filepath)
         for mapping in self._file_name_mapping.keys():
             if mapping.search(abspath) is not None:
                 return self._file_name_mapping[mapping]
